50da6f66c9f446f187bc9db975790f9c9bd046c0
crazyotakuu <42383388+crazyotakuu@users.noreply.github.com>
2020-11-21 00:53:23 +0530

    added location and More UI

--- app/src/main/java/com/example/feedthehunger/Donor.java
+++ app/src/main/java/com/example/feedthehunger/Donor.java
@@ -2,13 +2,23 @@ package com.example.feedthehunger;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toolbar;
 
 import com.google.android.material.tabs.TabLayout;
@@ -16,33 +26,74 @@ import com.google.android.material.tabs.TabLayout;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Donor extends AppCompatActivity {
+public class Donor extends AppCompatActivity implements LocationListener {
     private ViewPager viewpager;
     private TabLayout tablayout;
     private addDonation addDonation;
     private checkStatus checkStatus;
     private checkHistory checkHistory;
+    LocationManager locationManager;
+    double latitude,longitude;
 
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donor);
 
-        viewpager=findViewById(R.id.viewpager);
-        tablayout=findViewById(R.id.tablayout);
-        addDonation=new addDonation();
-        checkStatus=new checkStatus();
-        checkHistory=new checkHistory();
+        viewpager = findViewById(R.id.viewpager);
+        tablayout = findViewById(R.id.tablayout);
+        addDonation = new addDonation();
+        checkStatus = new checkStatus();
+        checkHistory = new checkHistory();
         tablayout.setupWithViewPager(viewpager);
-        ViewPagerAdapter viewpageadapter=new ViewPagerAdapter(getSupportFragmentManager(),0);
+        ViewPagerAdapter viewpageadapter = new ViewPagerAdapter(getSupportFragmentManager(), 0);
 
-        viewpageadapter.addFragment(addDonation,"Donation");
-        viewpageadapter.addFragment(checkStatus,"Weight");
-        viewpageadapter.addFragment(checkHistory,"Nearby");
+        viewpageadapter.addFragment(addDonation, "Donation");
+        viewpageadapter.addFragment(checkStatus, "Weight");
+        viewpageadapter.addFragment(checkHistory, "Nearby");
         viewpager.setAdapter(viewpageadapter);
         tablayout.getTabAt(0).setIcon(R.drawable.ic_donation);
         tablayout.getTabAt(1).setIcon(R.drawable.ic_status);
         tablayout.getTabAt(2).setIcon(R.drawable.ic_clock);
+        //location code starts from here
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, (LocationListener) this);
+
+    }
+    @Override
+    public void onLocationChanged(Location location) {
+//        txtLat = (TextView) findViewById(R.id.textview1);
+//        txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+        latitude=location.getLatitude();
+        longitude=location.getLongitude();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Latitude","disable");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Latitude","enable");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Latitude","status");
     }
 
 
